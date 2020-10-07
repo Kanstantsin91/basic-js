3779f5b5e4854f72cab30bb8eb0a0e906166b7d2
Kanstantsin Sakavets <kanstantsin.sakavets@gmail.com>
2020-10-07 21:25:38 +0300

    feat: implement transform-array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('Error');
+  let transformedArr = [...arr];
+  transformedArr.forEach( (el, index)=>{
+  switch (el) {
+  	case '--discard-next':
+    	transformedArr[index+1] = undefined;
+      transformedArr[index] = undefined;
+      break;
+    case '--discard-prev':
+    	transformedArr[index-1]  = undefined;
+      transformedArr[index] = undefined;
+      break;
+    case '--double-next':
+			transformedArr = [...transformedArr.slice(0, index), transformedArr[index+1], ...transformedArr.slice(index+1)];   
+      break;
+    case '--double-prev':
+    	transformedArr = [...transformedArr.slice(0, index), transformedArr[index-1], ...transformedArr.slice(index+1)];
+  }
+  });
+  return transformedArr.filter((el)=> el != undefined);
 };
