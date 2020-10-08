1a4bd036f10e5a8ada3417abdbe93f1af579105f
Kanstantsin Sakavets <kanstantsin.sakavets@gmail.com>
2020-10-09 01:04:34 +0300

    fix: extended-repeater code fix

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,35 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  let {repeatTimes = 1, separator = '+', addition, additionRepeatTimes = 1, additionSeparator = '|'} = options;
-  if(typeof str != 'string'){
-  	str = String(str);
+  let {
+    repeatTimes = 1, separator = '+', addition, additionRepeatTimes = 1, additionSeparator = '|'
+  } = options;
+  if (typeof str != 'string') {
+    str = String(str);
   }
-  
-  if(typeof addition != 'string') {
-  	addition = String(addition);
+
+  if (typeof addition != 'string') {
+    addition = String(addition);
   }
-  
-  if(!repeatTimes){
-  	return str;
+
+  if (!repeatTimes) {
+    return str;
   }
   let fullStr = str;
   let fullAddition = '';
   let returnedStr = '';
-  if (addition !== 'undefined'){
-  for(let i = 0; i < ((additionRepeatTimes*2) - 1); i++){
-  		if( i % 2 === 0 ){
-      	fullAddition += addition;
+  if (addition !== 'undefined') {
+    for (let i = 0; i < ((additionRepeatTimes * 2) - 1); i++) {
+      if (i % 2 === 0) {
+        fullAddition += addition;
       } else fullAddition += additionSeparator;
-  	}
+    }
     fullStr += fullAddition;
   }
-  for(let i = 0; i < ((repeatTimes*2) - 1); i++){
-  		if( i % 2 === 0){
-      	returnedStr += fullStr;
-      } else returnedStr += separator;
-  	}
-    
+  for (let i = 0; i < ((repeatTimes * 2) - 1); i++) {
+    if (i % 2 === 0) {
+      returnedStr += fullStr;
+    } else returnedStr += separator;
+  }
+
   return returnedStr;
 };
-  
