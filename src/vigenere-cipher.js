d48aef90cc25645e60bd76fcfbfb4d33cdedc562
Kanstantsin Sakavets <kanstantsin.sakavets@gmail.com>
2020-10-11 17:36:41 +0300

    feat: implement vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,63 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
+
+  encrypt(phrase, key) {
+    let encryptedPhrase = '';
+
+    key = key.toUpperCase().repeat(Math.ceil(phrase.length / key.length));
+    phrase = phrase.toUpperCase();
+
+    let phraseCounter = 0;
+    let keyCounter = 0;
+
+    while (phraseCounter < phrase.length) {
+      if (phrase.charCodeAt(phraseCounter) >= 65 && phrase.charCodeAt(phraseCounter) <= 90) {
+        encryptedPhrase += String.fromCharCode(((phrase.charCodeAt(phraseCounter) - 65 + key.charCodeAt(keyCounter) - 65) % 26) + 65)
+        phraseCounter++;
+        keyCounter++;
+      } else {
+        encryptedPhrase += phrase[phraseCounter];
+        phraseCounter++;
+      };
+    };
+
+    if (this.direct) {
+      return encryptedPhrase;
+    } else {
+      return encryptedPhrase.split('').reverse().join('');
+    };
+  }
+
+  decrypt(phrase, key) {
+    let decryptedPhrase = '';
+    key = key.toUpperCase().repeat(Math.ceil(phrase.length / key.length));
+
+    phrase = phrase.toUpperCase();
+
+    let phraseCounter = 0;
+    let keyCounter = 0;
+
+    while (phraseCounter < phrase.length) {
+      if (phrase.charCodeAt(phraseCounter) >= 65 && phrase.charCodeAt(phraseCounter) <= 90) {
+        decryptedPhrase += String.fromCharCode(((phrase.charCodeAt(phraseCounter) + 26 - key.charCodeAt(keyCounter)) % 26) + 65);
+        phraseCounter++;
+        keyCounter++;
+      } else {
+        decryptedPhrase += phrase[phraseCounter];
+        phraseCounter++;
+      };
+    };
+
+    if (this.direct) {
+      return decryptedPhrase;
+    } else {
+      return decryptedPhrase.split('').reverse().join('');
+    };
+  };
 }
 
 module.exports = VigenereCipheringMachine;
